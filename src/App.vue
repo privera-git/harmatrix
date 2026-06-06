64e5d4490e6f6eb4032935419e002defcdca364d
Patricio Rivera <patricio_rivera@outlook.com>
2026-06-06 15:08:31 -0400

    feat: replace scaffold with phase-based view shell (#25)
    - Remove Vue default scaffold (HelloWorld, TheWelcome, WelcomeItem, icons)
    - Replace App.vue with v-if shell driven by gameStore.session.phase
    - Add IdleView, PlayingView, CompletedView as empty stubs
    - Replace HelloWorld.spec.ts with App smoke test (idle phase default)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { storeToRefs } from 'pinia'
+import { useGameStore } from '@/stores/game'
+import IdleView from '@/components/IdleView.vue'
+import PlayingView from '@/components/PlayingView.vue'
+import CompletedView from '@/components/CompletedView.vue'
+
+const { session } = storeToRefs(useGameStore())
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <IdleView v-if="session.phase === 'idle'" />
+  <PlayingView v-else-if="session.phase === 'playing'" />
+  <CompletedView v-else-if="session.phase === 'completed'" />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
